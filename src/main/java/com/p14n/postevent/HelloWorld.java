cb8f1f720e5a91c0803261ded7be474fcd7b674e
Brendan Maginnis <brendan.maginnis@gmail.com>
2024-10-29 09:07:59 +0000

    add hello world class

---
+++ src/main/java/com/p14n/postevent/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.p14n.postevent;
+
+public class HelloWorld {
+    public static String getMessage() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
