dfc2df90f806eae5fe9db2a54a9913f14bf778cb
Dean Chapman <dean.chapman@kroo.com>
2025-04-26 08:29:11 +0100

    [human] Switch DST back on

--- library/src/test/java/com/p14n/postevent/dst/DeterministicConsumerTest.java
+++ library/src/test/java/com/p14n/postevent/dst/DeterministicConsumerTest.java
@@ -25,7 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
-@Disabled
 class DeterministicConsumerTest {
     private static final Logger logger = LoggerFactory.getLogger(DeterministicConsumerTest.class);
     private static final int PORT = 50052;
